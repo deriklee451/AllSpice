8d21eba0c22470df6e31b5580ebea2dab07c9fea
Derik Banning <deriklee451@gmail.com>
2023-01-20 16:15:44 -0700

    EOD commit

--- Services/RecipesService.cs
+++ Services/RecipesService.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace AllSpice.Services
 {
     public class RecipesService
     {
-        
+        internal List<Recipe> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
