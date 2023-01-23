734e6aa2c816393e8a8feb336e24071c91caa7a2
Derik Banning <deriklee451@gmail.com>
2023-01-23 08:55:23 -0700

    Not sure what this commit is.

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -1,26 +1,44 @@
-using Microsoft.Extensions.Logging;
 
-namespace AllSpice.Controllers
+
+namespace AllSpice.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RecipeController : ControllerBase
 {
-    [Route("[controller]")]
-    public class RecipeController : Controller
+    private readonly RecipesService _recipesService;
+
+    private readonly Auth0Provider _auth0provider;
+
+    private readonly IngredientService _ingredientService;
+
+    private readonly FavoritesService _favoritesService;
+
+    public RecipeController(RecipesService recipesService, Auth0Provider auth0provider, IngredientService ingredientService, FavoritesService favoritesService)
     {
-        private readonly ILogger<Recipe> _logger;
+        _recipesService = recipesService;
+        _auth0provider = auth0provider;
+        _ingredientService = ingredientService;
+        _favoritesService = favoritesService;
+    }
 
-        public RecipeController(ILogger<Recipe> logger)
+    [HttpGet]
+    public async Task<ActionResult<List<Recipe>>> Get()
+    {
+        try
         {
-            _logger = logger;
+            Account userInfo = await _auth0provider.GetUserInfoAsync<Account>(HttpContext);
+            List<Recipe> recipes = _recipesService.Get(userInfo?.Id);
+            return Ok(recipes);
         }
-
-        public IActionResult Index()
+        catch (Exception e)
         {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
+            return BadRequest(e.Message);
         }
     }
+
+
+
+
 }
