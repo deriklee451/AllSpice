9c2796feeddc5620960c303b7d28f688d680cd44
Derik Banning <deriklee451@gmail.com>
2023-01-25 12:25:00 -0700

    Favorite commit

--- Services/FavoritesService.cs
+++ Services/FavoritesService.cs
@@ -1,12 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Allspice.Services;
 
-namespace AllSpice.Services
+public class FavoritesService
 {
-    public class FavoritesService
+    private readonly FavoriteRepository _repo;
+
+    public FavoritesService(FavoriteRepository repo)
     {
-        
+        _repo = repo;
+    }
+
+    internal Favorite GetById(int id)
+    {
+        Favorite found = _repo.GetById(id);
+        if (found == null)
+        {
+            throw new Exception("Invalid Id");
+        }
+        return found;
+    }
+
+    internal Favorite Create(Favorite favoriteData)
+    {
+        Favorite found = _repo.foundFavorite(favoriteData);
+        if (found != null)
+        {
+            return found;
+        }
+        return _repo.Create(favoriteData);
+    }
+
+    internal void Delete(int id, string userId)
+    {
+        Favorite toDelete = GetById(id);
+        if (toDelete.AccountId != userId)
+        {
+            throw new Exception("Unable to delete");
+        }
+        _repo.Delete(id);
     }
 }
